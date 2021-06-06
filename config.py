0ce5d269069db9842f02c031d4e1c05dbfc138d3
Prince Patel <46424040+20prince12@users.noreply.github.com>
2021-06-06 16:38:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
-TOKEN = os.environ.get('botToken')
-URL = os.environ.get('botURL')
+TOKEN ="1640025618:AAGH6KpP9PaYW0552EzKqVCEslUkF40_sTI"
+URL = "https://strangerchat.azurewebsites.net"
 BASE_TELEGRAM_URL = 'https://api.telegram.org/bot{}'.format(TOKEN)
 LOCAL_WEBHOOK_ENDPOINT = '{}/webhook'.format(URL)
 TELEGRAM_INIT_WEBHOOK_URL = '{}/setWebhook?url={}'.format(BASE_TELEGRAM_URL, LOCAL_WEBHOOK_ENDPOINT)
